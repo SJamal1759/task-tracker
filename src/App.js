180aa545da9120f7fc4ca6ca9532d7d64e600b79
SJamal1759 <sjamal1759@gmail.com>
2023-11-22 16:34:42 +0300

    Task Updates

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 
-
+import { useState } from "react";
 import Header from './components/Header';
 import Tasks from './components/Tasks';
 
 function App() {
-  const name = 'Jamal'
+  const [tasks,setTasks] = useState([
+    {
+        id:1,
+        text:'school homework',
+        day:'Feb 5th at 3:20pm',
+        reminder:true,
+      },
+      {
+        id:2,
+        text:'office work',
+        day:'Feb 5th at 3:20pm',
+        reminder:true,
+      },
+      {
+        id:3,
+        text:'kids holiday homework',
+        day:'Feb 5th at 3:20pm',
+        reminder:false,
+      },
+    ])
+  
   return (
     <div className="container">
      <Header/>
+     <Tasks tasks={tasks}/>
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,32 +1,16 @@
-import React from 'react'
 
-const task = [{
-  id:1,
-  text:'school homework',
-  day:'Feb 5th at 3:20pm',
-  reminder:true,
-},
-{
-  id:2,
-  text:'office work',
-  day:'Feb 5th at 3:20pm',
-  reminder:true,
-},
-{
-  id:3,
-  text:'kids holiday homework',
-  day:'Feb 5th at 3:20pm',
-  reminder:false,
-}
-]
-const Tasks = () => {
+
+
+const Tasks = ({ tasks }) => {
+  
   return (
     <>
-      {Tasks.map((task) =>{
-        <h3>{task.text}</h3>
-      })}
+      {tasks.map((task) => (
+        <h3 key={task.id}> {task.text}</h3>
+
+      ))}
     </>
   )
 }
 
-export default Tasks
+export default Tasks;
